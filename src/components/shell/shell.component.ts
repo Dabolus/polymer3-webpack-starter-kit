c685215199ad508eb303f63d86ba6a0cbba77d86
John <john@themesurgeons.com>
2018-03-23 20:32:52 -0500

    Minor edit  in import statement.

--- src/components/shell/shell.component.ts
+++ src/components/shell/shell.component.ts
@@ -1,4 +1,4 @@
-import {PolymerElement, html} from '@polymer/polymer/polymer-element';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
 import '@polymer/app-layout/app-drawer/app-drawer';
 import '@polymer/app-layout/app-drawer-layout/app-drawer-layout';
 import '@polymer/app-layout/app-header/app-header';
